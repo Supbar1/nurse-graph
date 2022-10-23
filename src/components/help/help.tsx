c2488a08c21104a2d0ed67f001c2a690dfc12322
Bartosz Kukier <supbar@wp.pl>
2022-10-23 17:38:45 +0200

    commit before removing of buttonContext

--- src/components/help/help.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: min(60vw, 850px);
-`;
-
-const Help = () => <Container>Help</Container>;
-
-export default Help;
--- src/components/notFound.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  width: min(60vw, 850px);
-`;
-const NotFound = () => <Container>NOT FOUND</Container>;
-
-export default NotFound;
