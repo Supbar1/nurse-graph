8970840f97be347521cb0dc7731b2e14f2ee4ef0
Bartosz Kukier <supbar@wp.pl>
2022-10-24 13:47:29 +0200

    git test again

--- src/NewNursesList.tsx
+++ src/NewNursesList.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+// import React, { useEffect, useState } from "react";
+// import axios from "axios";
 import config from "./services/config.json";
 import http from "./services/httpService";
 
 const fetchRandomData = async () => {
-  return axios
-    .get("https:/randomuser.me/api")
-    .then(({ data }) => {
-      return data;
-    })
-    .catch((error) => {
-      console.log(error.message);
-    });
+  // return axios
+  //   .get("https:/randomuser.me/api")
+  //   .then(({ data }) => {
+  //     return data;
+  //   })
+  //   .catch((error) => {
+  //     console.log(error.message);
+  //   });
 };
 const getFullUserName = (userInfo: any) => {
   const {
@@ -22,32 +22,32 @@ const getFullUserName = (userInfo: any) => {
 };
 
 const NewNursesList = () => {
-  const [userInfos, setUserInfos] = useState([]);
+  // const [userInfos, setUserInfos] = useState([]);
 
-  const [randomNurses, setRandomNurses] = useState([]);
+  // const [randomNurses, setRandomNurses] = useState([]);
 
-  const apiNurses = async (): Promise<any> => {
-    const { data } = await http.get(config.apiNurses);
-    const { results } = data;
-    console.log(...results);
+  // const apiNurses = async (): Promise<any> => {
+  //   const { data } = await http.get(config.apiNurses);
+  //   const { results } = data;
+  //   console.log(...results);
 
-    let container: any = [];
-    for (let i = 0; i <= 3; i++) {
-      fetchRandomData().then(({ results }) => {
-        container.push(...results);
-      });
-      setRandomNurses(container);
-    }
-  };
+  //   let container: any = [];
+  //   for (let i = 0; i <= 3; i++) {
+  //     fetchRandomData().then(({ results }) => {
+  //       container.push(...results);
+  //     });
+  //     setRandomNurses(container);
+  //   }
+  // };
 
-  useEffect(() => {
-    apiNurses();
+  // useEffect(() => {
+  //   apiNurses();
 
-    fetchRandomData().then((randomData) => {
+  //   fetchRandomData().then((randomData) => {
       //   setRandomUserData(JSON.stringify(randomData) || "No user Data");
-      setUserInfos(randomData.results);
-    });
-  }, []);
-  return randomNurses;
+  //     setUserInfos(randomData.results);
+  //   });
+  // }, []);
+  // return randomNurses;
 };
 export default NewNursesList;
